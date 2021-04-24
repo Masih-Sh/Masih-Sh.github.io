0890c3206e26ea0bcb91448535670a9a5105f813
Masih Shafieian <masih.shafieian@gmail.com>
2021-04-24 21:55:16 +0100

    sound effect error fixed 4

--- js/script.js
+++ js/script.js
@@ -280,7 +280,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
  // Typical action to be performed when the document is ready:
- document.getElementById("demo").innerHTML = xhttp.responseText;
+ //document.getElementById("demo").innerHTML = xhttp.responseText;
  var response = JSON.parse(xhttp.responseText);
  console.log(response.FireEquipment[0].power);
 }
